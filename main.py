a7b66d943c34bcd7a5c4ccd1e702d692aaa220ef
divergent-rose <zelman.natalie@gmail.com>
2026-01-27 12:15:40 -0800

    added  player import

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 
 
@@ -12,7 +14,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0 #set fps to 60
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         log_state() #log state 
@@ -20,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") #backround in black
+        player.draw(screen)
         pygame.display.flip() #screen refresh 
         dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time
 
