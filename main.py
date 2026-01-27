595e70a109e10a7c34babbe05966d1d93ba69101
divergent-rose <zelman.natalie@gmail.com>
2026-01-27 09:47:48 -0800

    Removed greeting print

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    print("Hello from a new commit!")
 
 
 if __name__ == "__main__":
