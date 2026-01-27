13845ec5bf2d01103be4d5a1b50a0270b0f85c3d
divergent-rose <zelman.natalie@gmail.com>
2026-01-27 13:57:19 -0800

    new functions

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
+import constants
+import pygame
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "gray", self.position, self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
 
     def update(self, dt):
         self.position += self.velocity * dt
